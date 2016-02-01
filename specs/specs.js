a41d2038c7616769f173b7675af20caaf12e2227
Michael Schennum <mikeschenphoto@gmail.com>
2016-02-01 15:33:31 -0800

    fixed specs

--- specs/specs.js
+++ specs/specs.js
@@ -1,7 +1,7 @@
 
 describe('Hike', function() {
-  it("creates a new hike suggestion with the given drive, distance, views, popularity, difficulty", function() {
-    var testHike =  new Hike(10, 5, "waterfall", "medium", "hard");
+  it("creates a new hike suggestion with the given drive, distance, popularity, difficulty", function() {
+    var testHike =  new Hike(10, 5, "medium", "hard");
     expect(testHike.drive).to.equal(10);
     expect(testHike.distance).to.equal(5);
     expect(testHike.views).to.equal("waterfall");
