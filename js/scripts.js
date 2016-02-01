a25b8123504bd62c1f815bc1f6a2559a5f893083
Nathan Otto <ottoetc@gmail.com>
2016-02-01 13:49:06 -0800

    add distance and drive prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,35 @@
 function Hike(drive, distance, views, popularity, difficulty) {
   this.drive = drive;
-  this. distance = distance;
+  this.distance = distance;
   this.views = views;
   this.popularity = popularity;
   this.difficulty = difficulty;
 }
 
-Hike.prototype.views = function() {
-	return;
-};
+Hike.prototype.drive = function() {
+  var drive = "";
+  if(drive < 10) {
+    drive = short;
+  }
+  else if(drive < 35) {
+    drive = medium;
+  }
+  else {
+    drive = long;
+  }
+  return drive;
+}
+
+Hike.prototype.distance = function() {
+  var distance = "";
+  if(distance <= 3) {
+    distance = short;
+  }
+  else if(distance <= 8) {
+    distance = medium;
+  }
+  else {
+    distance = long;
+  }
+  return distance;
+}
