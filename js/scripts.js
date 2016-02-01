2fa6c01b7bcb64ba2327983042e4e9b3a95b919f
Nathan Otto <ottoetc@gmail.com>
2016-02-01 15:29:31 -0800

    fix typo

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 function Hike(drive, distance, views, popularity, difficulty) {
   this.drive = drive;
   this.distance = distance;
-  this.views = views;
   this.popularity = popularity;
   this.difficulty = difficulty;
 }
