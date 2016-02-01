493200bdeb3a35c13572acb1da4b77dcbaa33d04
Michael Schennum <mikeschenphoto@gmail.com>
2016-02-01 09:20:27 -0800

    adds basic hike object with basic parameters

--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,11 @@
 
+describe('Hike', function() {
+  it("creates a new hike suggestion with the given drive, distance, views, popularity, difficulty", function() {
+    var testHike =  new Hike(10, 5, "waterfall", "medium", "hard");
+    expect(testHike.drive).to.equal(10);
+    expect(testHike.distance).to.equal(5);
+    expect(testHike.views).to.equal("waterfall");
+    expect(testHike.popularity).to.equal("medium");
+    expect(testHike.difficulty).to.equal("hard");
+  });
+});
