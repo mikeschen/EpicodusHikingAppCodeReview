e3776529223317c2a390faf0c0b1e0e2d7e89286
Michael Schennum <mikeschenphoto@gmail.com>
2016-02-01 17:22:55 -0800

    adds comparative loop array

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,20 @@
-function Hike(drive, distance, views, popularity, difficulty) {
-  this.drive = drive;
-  this.distance = distance;
-  this.views = views;
-  this.popularity = popularity;
-  this.difficulty = difficulty;
+var testHike = ["medium", "short", "high", "medium"];
+var testHike2 = ["short", "medium", "medium", "high"];
+
+var multnomahs = ["medium", "short", "high", "medium"];
+var macleays = ["short", "medium", "medium", "medium"];
+var tryon = ["short", "short", "high", "easy"];
+var hikeArray = [multnomahs, macleays];
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
 
-Hike.prototype.drive = function() {
-  var drive = "";
-  if(drive < 10) {
-    drive = "short";
-  }
-  else if(drive < 35) {
-    drive = "medium";
-  }
-  else {
-    drive = "long";
-  }
-  return drive;
-}
-
-Hike.prototype.distance = function() {
-  var distance = "";
-  if(distance <= 3) {
-    distance = "short";
-  }
-  else if(distance <= 8) {
-    distance = "medium";
-  }
-  else {
-    distance = "long";
-  }
-  return distance;
-}
-
-Hike.prototype.hikeFinder = function() {
-
-	var finalDestination = [];
-
-	if (this.drive === "medium"  && this.distance === "short" && this.views === "waterfall" && this.popularity === "high" && this.difficulty === "medium") {
-
-		var i = 0;
-		for(i in multnomah) {
-			if (multnomah.hasOwnProperty(i)) {
-			finalDestination.push(i + ":" + multnomah[i]);  //add .append here
-			}
-		}
-		return finalDestination;
-	}
-};
-
-var multnomah = new Hike("medium", "short", "waterfall", "high", "medium");
-
-var testHike = new Hike("medium", "short", "waterfall", "high", "medium");
-
-testHike.hikeFinder();
+arraysEqual(testHike, multnomahs);
